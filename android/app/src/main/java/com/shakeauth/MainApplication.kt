ec73f7ac3cf4534a28d26de43c58664b788f550b
Arispacco <paccotiktok37@gmail.com>
2026-05-26 22:42:32 +0000

    4e correction du workflow github pour build l'apk

--- android/app/src/main/java/com/shakeauth/MainApplication.kt
+++ android/app/src/main/java/com/shakeauth/MainApplication.kt
@@ -21,7 +21,7 @@ class MainApplication : Application(), ReactApplication {
               // add(MyReactNativePackage())
             }
 
-        override val jsMainModulePath: String = "index"
+        override fun getJSMainModuleName(): String = "index"
 
         override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
 
